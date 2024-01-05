d473dc8712d43f80d4428b97183c0352885a2e26
Ric Evans <ric@evans-jacquez.com>
2024-01-05 15:07:09 -0600

    fix import

--- tests/unit/test_watcher.py
+++ tests/unit/test_watcher.py
@@ -4,13 +4,13 @@
 import os
 from pathlib import Path
 
-import tms
+from tms import watcher
 
 
 async def test_000__watcher() -> None:
     """Test the watcher."""
     fpath = Path(os.environ["CI_TEST_JOB_EVENT_LOG_FILE"])
 
-    tms.watcher.watch_job_event_log(fpath)
+    watcher.watcher.watch_job_event_log(fpath)  # type: ignore[attr-defined]
 
     assert 0  # TODO
