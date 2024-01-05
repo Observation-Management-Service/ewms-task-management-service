cfaa830382388e163080b1dd0ccc38434476a452
Ric Evans <ric@evans-jacquez.com>
2024-01-05 15:09:37 -0600

    (debug)

--- tests/unit/test_watcher.py
+++ tests/unit/test_watcher.py
@@ -1,16 +1,19 @@
-"""Unit tests."""
+"""Unit tests for the watcher."""
 
 
 import os
 from pathlib import Path
 
-from tms import watcher
+import tms
 
 
-async def test_000__watcher() -> None:
+async def test_000() -> None:
     """Test the watcher."""
     fpath = Path(os.environ["CI_TEST_JOB_EVENT_LOG_FILE"])
 
-    watcher.watcher.watch_job_event_log(fpath)  # type: ignore[attr-defined]
+    print(dir(tms))
+    print(dir(tms.watcher))
+
+    watcher.watcher.watch_job_event_log(fpath)
 
     assert 0  # TODO
