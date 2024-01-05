ca444aa3ab007e3bf6bf283c8ba640b230e6b2fa
Ric Evans <ric@evans-jacquez.com>
2024-01-05 16:44:33 -0600

    another!

--- tests/unit/test_watcher.py
+++ tests/unit/test_watcher.py
@@ -3,7 +3,7 @@
 
 import os
 from pathlib import Path
-from unittest.mock import Mock
+from unittest.mock import AsyncMock, MagicMock
 
 from tms.watcher import watcher
 
@@ -12,6 +12,8 @@ async def test_000() -> None:
     """Test the watcher."""
     fpath = Path(os.environ["JOB_EVENT_LOG_DIR"]) / "condor_test_logfile"
 
-    await watcher.watch_job_event_log(fpath, Mock())
+    rc = MagicMock()
+    rc.request = AsyncMock(side_effect={})
+    await watcher.watch_job_event_log(fpath, rc)
 
     assert 0  # TODO
