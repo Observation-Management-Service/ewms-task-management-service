6f757b4456534909637eb9d33df23ea95c1c5240
Ric Evans <ric@evans-jacquez.com>
2024-01-05 16:25:24 -0600

    pt 2

--- tests/unit/test_watcher.py
+++ tests/unit/test_watcher.py
@@ -9,13 +9,15 @@ from unittest.mock import AsyncMock, Mock
 from tms.watcher import watcher
 
 
-@mock.patch("rest_tools.client.RestClient")
-async def test_000(rc_mock: Mock) -> None:
+@mock.patch("rest_tools.client.RestClient", new=Mock())
+async def test_000() -> None:
     """Test the watcher."""
     fpath = Path(os.environ["JOB_EVENT_LOG_DIR"]) / "condor_test_logfile"
 
-    rc_mock.request.side_effect = AsyncMock(return_value={})
-
-    await watcher.watch_job_event_log(fpath)
+    with mock.patch(
+        "rest_tools.client.RestClient.request",
+        side_effect=AsyncMock(return_value={}),
+    ):
+        await watcher.watch_job_event_log(fpath)
 
     assert 0  # TODO
