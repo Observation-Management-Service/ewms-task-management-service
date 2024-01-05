5aa09d822827da00510a31995cab98daad723e2b
Ric Evans <ric@evans-jacquez.com>
2024-01-05 15:39:52 -0600

    mock rest client

--- tests/unit/test_watcher.py
+++ tests/unit/test_watcher.py
@@ -9,8 +9,8 @@ from unittest.mock import Mock
 from tms.watcher import watcher
 
 
-@mock.patch("rest_tools.client.RestClient.request")
-async def test_000(req_mock: Mock) -> None:
+@mock.patch("rest_tools.client.RestClient")
+async def test_000(rc_mock: Mock) -> None:
     """Test the watcher."""
     fpath = Path(os.environ["JOB_EVENT_LOG_DIR"]) / "condor_test_logfile"
 
