c1463a0bd053fc3f16905234c41f42b7b5a3d2f2
Ric Evans <ric@evans-jacquez.com>
2024-01-05 17:29:49 -0600

    pt 2

--- tests/unit/test_watcher.py
+++ tests/unit/test_watcher.py
@@ -13,6 +13,8 @@ async def test_000() -> None:
     """Test the watcher."""
     fpath = Path(os.environ["JOB_EVENT_LOG_DIR"]) / "condor_test_logfile"
 
+    # TODO - update file in background task
+
     rc = MagicMock()
     rc.request = AsyncMock(return_value={})
     await watcher.watch_job_event_log(fpath, rc)
