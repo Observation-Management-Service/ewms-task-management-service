d698526b16cc7c2a38228c1e400f50af3f8d93e1
Ric Evans <ric@evans-jacquez.com>
2024-01-05 15:13:44 -0600

    got it

--- tests/unit/test_watcher.py
+++ tests/unit/test_watcher.py
@@ -4,16 +4,13 @@
 import os
 from pathlib import Path
 
-import tms
+from tms.watcher import watcher
 
 
 async def test_000() -> None:
     """Test the watcher."""
     fpath = Path(os.environ["CI_TEST_JOB_EVENT_LOG_FILE"])
 
-    print(dir(tms))
-    print(dir(tms.watcher))
-
-    watcher.watcher.watch_job_event_log(fpath)
+    await watcher.watch_job_event_log(fpath)
 
     assert 0  # TODO
