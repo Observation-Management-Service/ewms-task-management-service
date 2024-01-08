eb28668a58eea2e29fa72df00f2c2610e3909e91
Ric Evans <ric@evans-jacquez.com>
2024-01-08 15:04:34 -0600

    fix subsetting

--- tests/unit/test_watcher.py
+++ tests/unit/test_watcher.py
@@ -5,6 +5,7 @@ import asyncio
 import os
 import threading
 from pathlib import Path
+from typing import Iterator
 from unittest.mock import AsyncMock, MagicMock
 
 import htcondor  # type: ignore[import-untyped]
@@ -14,7 +15,16 @@ from tms.watcher import watcher
 LIVE_UPDATE_SLEEP = 2
 
 
+def _get_subset_job_event_log(lines: list[str], min_amount: int) -> Iterator[str]:
+    """Get a subset of the job event log and maintain valid syntax."""
+    for i, ln in enumerate(lines):
+        yield ln
+        if i >= min_amount and ln == "...\n":
+            return
+
+
 async def mimick_live_file_updates(src: Path, live_file: Path, n_updates: int) -> None:
+    """Routinely update the live file by iteratively adding lines from src."""
     with open(src) as f:  # thread safe b/c only reading
         lines = f.readlines()
 
@@ -22,7 +32,8 @@ async def mimick_live_file_updates(src: Path, live_file: Path, n_updates: int) -
         await asyncio.sleep(LIVE_UPDATE_SLEEP)
         with open(live_file, "w") as livef:
             amount = ((i + 1) / n_updates) * len(lines)
-            livef.write("".join(lines[: int(amount)]))
+            subset_lines = _get_subset_job_event_log(lines, int(amount))
+            livef.write("".join(subset_lines))
         with open(live_file) as livef:
             print(livef.read())  # TODO
 
