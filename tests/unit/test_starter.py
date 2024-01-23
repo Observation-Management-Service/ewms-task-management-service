9ed472459fa06d117d88398121abfc71a86e6deb
Ric Evans <ric@evans-jacquez.com>
2024-01-23 14:42:21 -0600

    fix spacing (4)

--- tests/unit/test_starter.py
+++ tests/unit/test_starter.py
@@ -55,8 +55,8 @@ async def test_000() -> None:
             config.ENV.JOB_EVENT_LOG_DIR / "tms-cluster-$(ClusterId)" / "$(ProcId).err"
         ),
         "transfer_output_files": (
-            f'"{str(config.ENV.JOB_EVENT_LOG_DIR / f"tms-{date.today()}.log")}, '
-            f'{str(config.ENV.JOB_EVENT_LOG_DIR / "tms-cluster-$(ClusterId)" / "$(ProcId).out")}, '
+            f'"{str(config.ENV.JOB_EVENT_LOG_DIR / f"tms-{date.today()}.log")},'
+            f'{str(config.ENV.JOB_EVENT_LOG_DIR / "tms-cluster-$(ClusterId)" / "$(ProcId).out")},'
             f'{str(config.ENV.JOB_EVENT_LOG_DIR / "tms-cluster-$(ClusterId)" / "$(ProcId).err")}"'
         ),  # must be quoted
     }
