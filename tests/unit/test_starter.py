e756be5adb7c60df9318f7d4ae5bafd6fc18db8a
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2024-03-12 16:02:25 -0500

    Starter: Make Output Subdir - 2 (#15)
    * Starter: Make Output Subdir - 2
    
    * update test

--- tests/unit/test_starter.py
+++ tests/unit/test_starter.py
@@ -70,12 +70,12 @@ async def test_000(htcs_mock: MagicMock, itsps_mock: AsyncMock) -> None:
         #
         "output": str(
             config.ENV.JOB_EVENT_LOG_DIR
-            / "ewms-cluster-$(ClusterId)-taskforce-$(EWMSTaskforceUUID)"
+            / "ewms-cluster-$(ClusterId)-taskforce-9874abcdef"
             / "$(ProcId).out"
         ),
         "error": str(
             config.ENV.JOB_EVENT_LOG_DIR
-            / "ewms-cluster-$(ClusterId)-taskforce-$(EWMSTaskforceUUID)"
+            / "ewms-cluster-$(ClusterId)-taskforce-9874abcdef"
             / "$(ProcId).err"
         ),
         "transfer_output_files": "",
