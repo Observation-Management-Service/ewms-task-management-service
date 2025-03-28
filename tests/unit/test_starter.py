b8d237cf7fb419ace7484440e68360ccbb4158c0
ric-evans <emejqz@gmail.com>
2025-03-28 13:26:30 -0500

    Exclude Sites: "AMNH" and "NotreDame" (plus a new helper script) - 2

--- tests/unit/test_starter.py
+++ tests/unit/test_starter.py
@@ -58,6 +58,8 @@ async def test_000(htcs_mock: MagicMock, itsps_mock: AsyncMock) -> None:
             'GLIDEIN_Site =!= "San Diego Supercomputer Center" && '
             'GLIDEIN_Site =!= "SDSC-PRP" && '
             'GLIDEIN_Site =!= "Kansas State University" && '
+            'GLIDEIN_Site =!= "AMNH" && '
+            'GLIDEIN_Site =!= "NotreDame" && '
             "(foo)"
         ),
         "+FileSystemDomain": '"blah"',  # must be quoted
