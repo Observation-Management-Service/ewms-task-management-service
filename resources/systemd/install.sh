11807058a66b0a5c77069f151b7bb6daac370894
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2025-04-02 12:59:09 -0500

    Spilt/Duplicate Systemd Config for `ewms-prod` + `ewms-dev` (#36)
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Alec <asheperd@icecube.wisc.edu>

---
+++ resources/systemd/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# install systmed unit files to user systemd
+
+SYSTEMD_INSTALL_DIR=$HOME/.config/systemd/user/
+
+ENVS=( tms-dev tms-prod )
+
+if [[ ! -d $SYSTEMD_INSTALL_DIR ]]; then
+    mkdir -p $SYSTEMD_INSTALL_DIR
+fi
+
+for ENV in tms-dev tms-prod; do
+    cp -ux $ENV/* $SYSTEMD_INSTALL_DIR
+done
+
+# reload daemon for new/updated unit file
+systemctl --user daemon-reload
+
+for ENV in tms-dev tms-prod; do
+    for UNIT in `ls $ENV`; do
+        systemctl --user enable  $UNIT
+
+        if systemctl --user show $UNIT --property=UnitFileState | grep -q  '=enabled'; then
+            echo "$UNIT: failed to enable"
+        else
+            echo "$UNIT: enabled"
+        fi
+    done
+done
