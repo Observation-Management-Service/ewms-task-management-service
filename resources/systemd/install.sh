9b1f862d29f20ac8e56e37f406dfb96f6487dfe8
ric-evans <emejqz@gmail.com>
2025-04-03 11:18:51 -0500

    update install.sh

--- resources/systemd/install.sh
+++ resources/systemd/install.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
+set -euo pipefail
 
+########################################################################################
 # install systmed unit files to user systemd
+#
+# Usage: ./install_units.sh <unit-subdir>
+########################################################################################
 
-SYSTEMD_INSTALL_DIR=$HOME/.config/systemd/user/
-
-ENVS=( tms-dev tms-prod )
-
-if [[ ! -d $SYSTEMD_INSTALL_DIR ]]; then
-    mkdir -p $SYSTEMD_INSTALL_DIR
+if [[ -z "${1:-}" ]]; then
+    echo "Usage: $0 <unit-subdir>" >&2
+    exit 1
+else
+    UNIT_SUBDIR=$1
 fi
 
-for ENV in tms-dev tms-prod; do
-    cp -ux $ENV/* $SYSTEMD_INSTALL_DIR
-done
+SYSTEMD_INSTALL_DIR="$HOME/.config/systemd/user"
 
-# reload daemon for new/updated unit file
-systemctl --user daemon-reload
 
-for ENV in tms-dev tms-prod; do
-    for UNIT in `ls $ENV`; do
-        systemctl --user enable  $UNIT
+########################################################################################
+# validate
 
-        if systemctl --user show $UNIT --property=UnitFileState | grep -q  '=enabled'; then
-            echo "$UNIT: failed to enable"
-        else
-            echo "$UNIT: enabled"
-        fi
-    done
+if [[ ! -d $UNIT_SUBDIR ]]; then
+    echo "ERROR: '$UNIT_SUBDIR' does not exist or is not a directory." >&2
+    exit 2
+fi
+
+
+########################################################################################
+# prep
+
+mkdir -p "$SYSTEMD_INSTALL_DIR"
+cp -ux "$UNIT_SUBDIR"/* "$SYSTEMD_INSTALL_DIR"  # don't overwrite existing ones or symlinks
+systemctl --user daemon-reload  # reload systemd to recognize new/updated unit files
+
+
+########################################################################################
+# enable unit files
+
+for UNIT_PATH in "$UNIT_SUBDIR"/*; do
+    UNIT=$(basename "$UNIT_PATH")
+    systemctl --user enable "$UNIT"
+
+    if systemctl --user show "$UNIT" --property=UnitFileState | grep -q '=enabled'; then
+        echo "$UNIT: enabled"
+    else
+        echo "$UNIT: failed to enable"
+    fi
 done
