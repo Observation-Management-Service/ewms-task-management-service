3a6069f1ccfe9e2e94f1e2eec8569876e8389c8e
ric-evans <emejqz@gmail.com>
2025-04-08 14:11:17 -0500

    Update `find_bad_sites.sh`

--- resources/find_bad_sites.sh
+++ resources/find_bad_sites.sh
@@ -2,17 +2,34 @@
 set -euo pipefail
 
 ########################################################################################
-# Usage: ./find_bad_sites.sh /scratch/ewms/tms/tms_scratchdir/ewms-taskforce-TF-[...]/cluster-[...]/ "error pattern"
+# Usage: ./find_bad_sites.sh /.../jobs/ TASKFORCE_UUID "error pattern"
+#   Example:
+#     ./find_bad_sites.sh /scratch/ewms/tms-foo/jobs TF-abc123 "Segmentation fault"
 ########################################################################################
 
-if [[ -z "${1-}" || -z "${2-}" ]]; then
-  echo "Usage: $0 CLUSTER_DIR 'error pattern'"
+
+if [[ -z "${1-}" || -z "${2-}" || -z "${3-}" ]]; then
+  echo "Usage: $0 TMS_JOBS_DIR TASKFORCE_UUID 'error pattern'"
   exit 1
 fi
 
-CLUSTER_DIR="$1"
-ERROR_PATTERN="$2"
-ROOT_DIR="/scratch/ewms/tms/tms_scratchdir"
+# validate & build paths
+if [[ -d "$1" && "$(basename "$1")" == "jobs" ]]; then
+    TMS_JOBS_DIR="$1"
+    # find exactly one matching cluster dir
+    readarray -t matches <<< "$(find "$TMS_JOBS_DIR/ewms-taskforce-TF-$2" -maxdepth 1 -type d -name 'cluster-*')"
+    if [[ ${#matches[@]} -ne 1 ]]; then
+        echo "ERROR: Expected exactly one cluster dir, found ${#matches[@]}"
+        exit 2
+    fi
+    CLUSTER_DIR="${matches[0]}"
+else
+    echo "ERROR: $1 is not a '.../jobs/' directory"
+    exit 2
+fi
+
+ERROR_PATTERN="$3"
+
 
 ########################################################################################
 # find all sites in cluster
@@ -64,13 +81,13 @@ fi
 # see if this site always fails with this error (all clusters)
 
 echo
-echo "[Step 4] Checking all jobs in $ROOT_DIR for $SITE with '$ERROR_PATTERN'..."
+echo "[Step 4] Checking all jobs in $TMS_JOBS_DIR for $SITE with '$ERROR_PATTERN'..."
 
-all_match_count=$(grep -rl --include='*.err' "$ERROR_PATTERN" "$ROOT_DIR" | \
+all_match_count=$(grep -rl --include='*.err' "$ERROR_PATTERN" "$TMS_JOBS_DIR" | \
   awk -F '.err' '{print $1".out"}' | \
   xargs -r grep "GLIDEIN_Site=$SITE" | wc -l)
 
-all_total_count=$(grep -r --include='*.out' "GLIDEIN_Site=$SITE" "$ROOT_DIR" | wc -l)
+all_total_count=$(grep -r --include='*.out' "GLIDEIN_Site=$SITE" "$TMS_JOBS_DIR" | wc -l)
 
 echo "  Total matched across all clusters: $all_match_count"
 echo "  Total jobs    across all clusters: $all_total_count"
