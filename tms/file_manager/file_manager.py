3f508f4771663c54ed378d4c0fbea3f8ecd9e8bc
ric-evans <emejqz@gmail.com>
2025-07-25 16:42:44 -0400

    class based

--- tms/file_manager/file_manager.py
+++ tms/file_manager/file_manager.py
@@ -8,6 +8,7 @@ import shutil
 import tarfile
 import time
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Literal
 
 LOGGER = logging.getLogger(__name__)
@@ -18,52 +19,72 @@ class FilepathAction:
     """What action to take on the filepath."""
 
     action: Literal["rm", "mv", "tar"]
-    min_age_secs: int  # Only act if file is older than this
+    age_threshold: int  # Only act if file is older than this
 
-    dest: str | None = None  # not all actions need destinations
+    dest: Path | None = None  # not all actions need destinations
+
+    def __post_init__(self):
+        if self.dest and self.dest.exists():
+            raise RuntimeError(f"destination already exists: {self.dest}")
+
+    def _rm(self, fpath: Path) -> None:
+        os.remove(fpath)
+        LOGGER.info(f"done: rm {fpath}")
+
+    def _mv(self, fpath: Path) -> None:
+        assert self.dest is not None
+        os.makedirs(self.dest, exist_ok=True)
+        shutil.move(fpath, self.dest)
+        LOGGER.info(f"done: mv {fpath} → {self.dest}")
+
+    def _tar(self, fpath: Path) -> None:
+        assert self.dest is not None
+        mode = "w:gz" if self.dest.endswith(".gz") else "w"
+        with tarfile.open(self.dest, mode) as tar:
+            tar.add(fpath, arcname=os.path.basename(fpath))
+        os.remove(fpath)
+        LOGGER.info(f"done: tar {fpath} → {self.dest} + rm {fpath}")
+
+    def is_old_enough(self, fpath: Path) -> bool:
+        """Is the filepath older than the age_threshold"""
+        return (time.time() - os.path.getmtime(fpath)) >= self.age_threshold
+
+    def act(self, fpath: Path) -> None:
+        """Perform action on filepath, if the file is old enough."""
+        if not fpath.exists():
+            raise FileNotFoundError(fpath)
+
+        if not self.is_old_enough(fpath):
+            return
+
+        actions = {
+            "rm": self._rm,
+            "mv": self._mv,
+            "tar": self._tar,
+        }
+
+        try:
+            actions[self.action](fpath)
+        except KeyError:
+            raise ValueError(f"Unknown action: {self.action}")
 
 
 ACTION_MAP: dict[str, FilepathAction] = {
-    "/tmp/data/*.log": FilepathAction("rm", 600),
-    "/tmp/data/to-move/*": FilepathAction("mv", 600, dest="/tmp/moved"),
-    "/tmp/data/to-archive/*": FilepathAction("tar", 1800, "/tmp/archives/data.tar.gz"),
+    "/tmp/data/*.log": FilepathAction(
+        "rm",
+        age_threshold=600,
+    ),
+    "/tmp/data/to-move/*": FilepathAction(
+        "mv",
+        age_threshold=600,
+        dest=Path("/tmp/moved"),
+    ),
+    "/tmp/data/to-archive/*": FilepathAction(
+        "tar",
+        age_threshold=1800,
+        dest=Path("/tmp/archives/data.tar.gz"),
+    ),
 }
-
-
-def is_old_enough(filepath: str, age_threshold: int) -> bool:
-    """Is the filepath older than the age_threshold"""
-    return (time.time() - os.path.getmtime(filepath)) >= age_threshold
-
-
-def process_action(fpath: str, fa: FilepathAction) -> None:
-    try:
-        if not is_old_enough(fpath, fa.min_age_secs):
-            return
-
-        match fa.action:
-            case "rm":
-                os.remove(fpath)
-                LOGGER.info(f"done: rm {fpath}")
-
-            case "mv":
-                assert fa.dest is not None
-                os.makedirs(fa.dest, exist_ok=True)
-                shutil.move(fpath, fa.dest)
-                LOGGER.info(f"done: mv {fpath} → {fa.dest}")
-
-            case "tar":
-                assert fa.dest is not None
-                mode = "w:gz" if fa.dest.endswith(".gz") else "w"
-                with tarfile.open(fa.dest, mode) as tar:
-                    tar.add(fpath, arcname=os.path.basename(fpath))
-                os.remove(fpath)
-                LOGGER.info(f"done: tar {fpath} → {fa.dest} + rm {fpath}")
-
-            case _:
-                LOGGER.warning(f"Unknown action: {fa.action}")
-
-    except Exception as e:
-        LOGGER.error(f"Error processing {fpath} with action={fa}: {e}")
 
 
 async def run() -> None:
@@ -74,4 +95,4 @@ async def run() -> None:
 
         for fpath_pattern, file_action in ACTION_MAP.items():
             for fpath in glob.glob(fpath_pattern):
-                process_action(fpath, file_action)
+                file_action.act(Path(fpath))
